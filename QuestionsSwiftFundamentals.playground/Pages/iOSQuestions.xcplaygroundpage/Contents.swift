f3f20b066d89df4df735d5297f96888a1caa3cdf
Juan C <juanceballos@pursuit.org>
2021-07-29 11:45:34 -0400

    first half of ios questions

---
+++ QuestionsSwiftFundamentals.playground/Pages/iOSQuestions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ 1) What are two native frameworks used to create user interface in ios?
+    1 - UIKIt, 2 - SwiftUI
+ 2) What does IB in IBOutlet or IBActiona stand for?
+ Interface Builder
+ 3) What are the two required methods of a UITableviewDataSource
+    1 - cellForRowAt, 2 - numberOfRowsInSection
+ 4) What's the difference between Push Notifications, and Local Notifications
+ Push Notifications are triggers from a server that are remotely sent to an iOS client application. Local Notifications are triggers for iOS delivered locally, the notifications are triggered namely by location, timer, and event calendar
+ 5) Name the ways to persist Data in iOS
+  1 - Core Data 2 - User Defaults 3 - Documents Directory
+ 6) What is Result Type
+ Result Type is an enum type with a success or failure case with respective associated values
+ 7) Describe the ways in which a view can be created
+    1 - Programmatically, 2 - UIStoryboard 3 - xib
+ 8) What is ARC?
+ ARC is Automatic Reference Counting - It allows the system to automatically handle most of the retain/release cycles, with one exception being closures which you have to use weak/unowned self
+ 
+ ARC is Automatic Reference Counting - With it the system can automatically handle most of the retain/release counts, with one exception being capturing closures which you have to use weak/unowned
+ */
+
